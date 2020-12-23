f9e3ec4156a8e1a44a155d6511bcfefe4a1faa3e
PhuocHo <phuochoquang97>
2020-12-23 15:12:27 +0700

    phan quyen chinh sua thong tin nguoi dung

--- WEB_SERVICE/src/main/java/controller/NguoiDungController.java
+++ WEB_SERVICE/src/main/java/controller/NguoiDungController.java
@@ -1,9 +1,9 @@
 package controller;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +17,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +26,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import model.GioiTinh;
 import model.NguoiDung;
+import model.TaiKhoan;
 import net.minidev.json.JSONObject;
 import service.NguoiDungService;
+import utils.GetTaiKhoanFromTokenService;
 
 @CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
@@ -35,6 +37,8 @@ import service.NguoiDungService;
 public class NguoiDungController {
 	@Autowired
 	private NguoiDungService nguoiDungService;
+	@Autowired
+	private GetTaiKhoanFromTokenService getTaiKhoanFromToken;
 
 	/*
 	 * API lay thong tin nguoi dung
@@ -44,13 +48,15 @@ public class NguoiDungController {
 	public ResponseEntity<JSONObject> getNguoiDung(@PathVariable Long maNguoiDung) {
 		JSONObject returnedObject = new JSONObject();
 		NguoiDung nguoiDung = nguoiDungService.findByDeletedFalse(maNguoiDung);
-		if (nguoiDung == null) return new ResponseEntity<JSONObject>(returnedObject, HttpStatus.NOT_FOUND);
+		if (nguoiDung == null)
+			return new ResponseEntity<JSONObject>(returnedObject, HttpStatus.NOT_FOUND);
 		returnedObject.put("hoTen", nguoiDung.getHoTen());
 		returnedObject.put("ngaySinh", nguoiDung.getNgaySinh());
 		returnedObject.put("sdt", nguoiDung.getSDT());
 		returnedObject.put("thanhPho", nguoiDung.getThanhPho());
 		returnedObject.put("anhDaiDien", nguoiDung.getAnhDaiDien());
-		returnedObject.put("gioiTinh", (nguoiDung.getGioiTinh() == null) ? nguoiDung.getGioiTinh().getTenGioiTinh():null);
+		returnedObject.put("gioiTinh",
+				(nguoiDung.getGioiTinh() == null) ? nguoiDung.getGioiTinh().getTenGioiTinh() : null);
 		return new ResponseEntity<JSONObject>(returnedObject, HttpStatus.OK);
 	}
 
@@ -62,34 +68,40 @@ public class NguoiDungController {
 	public ResponseEntity<Void> chinhSuaNguoiDung(@PathVariable Long maNguoiDung, @RequestParam("hoTen") String hoTen,
 			@RequestParam("ngaySinh") String ngaySinh, @RequestParam("maGioiTinh") Integer maGioiTinh,
 			@RequestParam("thanhPho") String thanhPho, @RequestParam("sdt") String sdt,
-			@RequestParam("anhDaiDien") MultipartFile multipartFile) {
+			@RequestParam("anhDaiDien") MultipartFile multipartFile, @RequestHeader("Authorization") String tokenJWT) {
+		TaiKhoan taiKhoan = getTaiKhoanFromToken.getTaiKhoan(tokenJWT);
 		NguoiDung nguoiDung = nguoiDungService.findByDeletedFalse(maNguoiDung);
-		nguoiDung.setHoTen(hoTen);
+		if (taiKhoan.getNguoiDung().equals(nguoiDung)) {
+			nguoiDung.setHoTen(hoTen);
 
-		DateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		java.sql.Date ngaySinh_convert = null;
-		try {
-			ngaySinh_convert = new Date(simpleDateFormat.parse(ngaySinh).getTime());
-		} catch (Exception e) {
-			e.printStackTrace();
+			DateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			java.sql.Date ngaySinh_convert = null;
+			try {
+				ngaySinh_convert = (java.sql.Date) new Date(simpleDateFormat.parse(ngaySinh).getTime());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			nguoiDung.setNgaySinh(ngaySinh_convert);
+			nguoiDung.setSDT(sdt);
+			nguoiDung.setThanhPho(thanhPho);
+
+			GioiTinh gioiTinh = new GioiTinh();
+			System.out.println("gioiTinh :" + gioiTinh);
+			gioiTinh.setMaGioiTinh(maGioiTinh);
+			nguoiDung.setGioiTinh(gioiTinh);
+
+			try {
+				nguoiDung.setAnhDaiDien(multipartFile.getBytes());
+			} catch (IOException e) {
+				return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+
+			nguoiDungService.save(nguoiDung);
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 		}
-		nguoiDung.setNgaySinh(ngaySinh_convert);
-		nguoiDung.setSDT(sdt);
-		nguoiDung.setThanhPho(thanhPho);
 
-		GioiTinh gioiTinh = new GioiTinh();
-		System.out.println("gioiTinh :" + gioiTinh);
-		gioiTinh.setMaGioiTinh(maGioiTinh);
-		nguoiDung.setGioiTinh(gioiTinh);
-
-		try {
-			nguoiDung.setAnhDaiDien(multipartFile.getBytes());
-		} catch (IOException e) {
-			return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		nguoiDungService.save(nguoiDung);
-		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 
 	/*
